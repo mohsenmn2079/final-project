c57e3856490c4629dfac2375d365a40fafa2807e
nic <mohsenmn2079@gmail.com>
2023-09-14 11:03:53 +0330

    implement spring basic security

---
+++ src/main/java/com/example/finalproject/user/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.finalproject.user.config;
+
+import com.example.finalproject.user.repository.UserRepository;
+import com.example.finalproject.user.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public CustomUserDetailsService userDetailsService(UserRepository userRepository) {
+        return new CustomUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomUserDetailsService customUserDetailsService) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+//                .userDetailsService(customUserDetailsService);
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/example/finalproject/user/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.finalproject.user.service;
+
+import com.example.finalproject.user.entity.User;
+import com.example.finalproject.user.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        for (User user : userRepository.findAll()) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        throw new UsernameNotFoundException("User not found");
+    }
+}
+
+
+
